35dc6e6ca77868b7dd62264be604e4c6c52a8adf
gpolt <>
2014-11-24 12:42:50 +0100

    some changes

--- src/gui/newgui.java
+++ src/gui/newgui.java
@@ -7,6 +7,7 @@ package gui;
 
 import javafx.application.Application;
 import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,6 +16,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.ProgressBar;
 
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -39,14 +41,31 @@ public class newgui extends Application {
         flowPane.getChildren().add(progressBar);
         
         for (int i = 0; i < 5; i++) {
-            Button button = new Button("Weiter");
+            Button button = new Button("Verschlüsseln");
             button.setDisable(true);
             flowPane.getChildren().add(button);
         }
         
+        
+        
         // Inhalt des Fensters
-        Label label = new Label("Das ist ein Text.");
-        Scene scene = new Scene(flowPane);
+        Label bottomLabel = new Label("Das ist ein Text.");
+        
+        //bottomLabel.
+        //bottomLabel.getChildrenUnmodifiable().add(encButton);
+        //bottomLabel.getChildrenUnmodifiable().add(decButton);
+        BorderPane borderPane = new BorderPane();
+        
+        Button encButton = new Button("Verschlüsseln");
+        Button decButton = new Button("Entschlüsseln");
+        
+        encButton.set
+        
+        borderPane.setTop(flowPane);
+        borderPane.setBottom(encButton);
+        //borderPane.bottomProperty().
+        
+        Scene scene = new Scene(borderPane);
         
         stage.setScene(scene);
         stage.show();
